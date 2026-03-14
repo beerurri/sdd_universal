4133d7a54559d8260544b247149091ccd266aa26
beerurri <i.v.sergeyev@gmail.com>
2026-03-14 18:49:24 +0300

    2d

--- py-scripts/plots/plot_2D.py
+++ py-scripts/plots/plot_2D.py
@@ -9,80 +9,189 @@ from check_results import parse_filename
 from build_filename import build_filename
 from check_results import remove_mc_duplicates
 
+fig, axs = plt.subplots(2, 3, constrained_layout=True)
+fig.delaxes(axs[1, 2])
+
 results = [parse_filename(file) for file in os.listdir(os.path.join(os.getcwd(), 'results'))]
 
-LCs = list()
-QCs = list()
-CCs = list()
-XCs = list()
-NRMSEs = list()
-NMSEs = list()
+# MC
 
-
-
-results = [
+mc_results = [
     res for res in results
     if res['task'] == 'mc' and
-    res['case'] == 'MC-THETA' and
+    res['case'] == 'MC-2D' and
     res['constant_theta'] == False and
-    res['every_second'] == False]
+    res['every_second'] == False
+]
 
-results = sorted(results, key=lambda x: x['theta'])
-results = remove_mc_duplicates(results)
-thetas = [d['theta'] for d in results]
+mc_results = remove_mc_duplicates(mc_results)
+mc_results = sorted(mc_results, key=lambda x: (x['theta'], x['beta']))
 
-mc_files = list() # 2d list
+betas = sorted({p['beta'] for p in mc_results})
+thetas = sorted({p['theta'] for p in mc_results})
+
+n_beta = len(betas)
+n_theta = len(thetas)
+
+
+def process_mc_file(file):
+    with open(os.path.join(os.getcwd(), 'results', file), 'r') as f:
+        source = f.read().strip('\n').split('\n')
+
+    tmp = np.array([np.fromstring(string, sep=',').sum() for string in source if string])
+
+    return tmp.mean() if len(tmp) else 0
+
+
+mcs = dict()
+
 for mc_type in ['LC', 'QC', 'CC', 'XC']:
-    mc_files.append([build_filename(res, mc_type=mc_type) for res in results])
+    Z = np.zeros((n_theta, n_beta))
 
-# print(f'{len(results)} vs {len(SF_tau_0)}')
+    for mc_res in mc_results:
+        file = build_filename(mc_res, mc_type=mc_type)
+        mean = process_mc_file(file)
 
-mean = list()
-std = list()
+        i = thetas.index(mc_res['theta'])
+        j = betas.index(mc_res['beta'])
 
-for mc_type_files in mc_files:
-    curr_mc_mean = list()
-    curr_mc_std = list()
-    for file in mc_type_files:
-        with open(os.path.join(os.getcwd(), 'results', file), 'r') as f:
-            source = f.read().strip('\n').split('\n')
-            
-        tmp = np.array([np.fromstring(string, sep=',').sum() for string in source if string])
-        curr_mc_mean.append(tmp.mean())
-        curr_mc_std.append(tmp.std())
-    
-    mean.append(np.array(curr_mc_mean))
-    std.append(np.array(curr_mc_std))
+        Z[i, j] = mean
 
-mean_sum = np.array(mean).sum(axis=0)
-mean_std = np.array(std).std(axis=0)
+    mcs[mc_type] = Z
 
-# fig = plt.figure(figsize=(6, 4), dpi=200)
+
+mcs['TC'] = mcs['LC'] + mcs['QC'] + mcs['CC'] + mcs['XC']
+
+extent = [
+    betas[0], betas[-1],
+    thetas[0], thetas[-1]
+]
+
+dx = (betas[-1] - betas[0]) / n_beta
+dy = (thetas[-1] - thetas[0]) / n_theta
+
+x_centers = betas[0] + dx * (np.arange(n_beta) + 0.5)
+y_centers = thetas[0] + dy * (np.arange(n_theta) + 0.5)
+
+for i, mc_type in enumerate(mcs.keys()):
+    ax = axs[i // 3, i % 3]
+    im = ax.imshow(
+        mcs[mc_type],
+        origin='lower',
+        aspect='auto',
+        extent=extent,
+        vmin=0
+    )
+    ax.set_title(mc_type)
+    ax.set_xlabel(r'$\beta$')
+    ax.set_ylabel(r'$\theta$')
+    ax.set_xticks(x_centers)
+    ax.set_xticklabels([f'{b:.2f}' for b in betas])
+
+    ax.set_yticks(y_centers)
+    ax.set_yticklabels([f'{t:.3f}' for t in thetas])
+    plt.colorbar(im, ax=ax)
+
+
+# NARMA
+
 fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax2 = ax1.twiny()
 
-for i, mc_type in enumerate(['LC', 'QC', 'CC', 'XC']):
-    ax1.plot(thetas, mean[i], '-', label=mc_type)
-    ax1.fill_between(thetas, mean[i] - std[i], mean[i] + std[i], alpha=0.3)
+narma_results = [
+    res for res in results
+    if res['task'] == 'narma' and
+    res['case'] == 'NARMA-2D' and
+    res['constant_theta'] == False and
+    res['every_second'] == False
+]
 
-ax1.plot(thetas, mean_sum, '-', label='sum')
-ax1.fill_between(thetas, mean_sum - mean_std, mean_sum + mean_std, alpha=0.3)
+betas = sorted({p['beta'] for p in narma_results})
+thetas = sorted({p['theta'] for p in narma_results})
 
-ax1.set_ylabel('$MC$')
+n_beta = len(betas)
+n_theta = len(thetas)
 
-ax1.set_xlabel('$\\Theta$')
-ax1.grid()
+narma_results = sorted(narma_results, key=lambda x: (x['theta'], x['beta']))
 
-def tick_func(x):
-    return [round(100 * _x / 1.2, 2) for _x in x]
+Z = np.zeros((n_theta, n_beta))
 
-new_tick_positions = [x for idx, x in enumerate(np.linspace(0, max(thetas), len(thetas), endpoint=True)) if idx % 5 == 0]
-# new_tick_positions = [0., .1, .2, .3, .4, .5, .6, .7, .8]
-ax2.set_xlim(ax1.get_xlim())
-ax2.set_xticks(new_tick_positions)
-ax2.set_xticklabels(tick_func(new_tick_positions))
-ax2.set_xlabel('$\\tau_0$')
+for narma_res in narma_results:
+    file = build_filename(narma_res)
 
-ax1.legend()
+    mean = np.loadtxt(os.path.join(os.getcwd(), 'results', file), delimiter=',').mean()
+
+    i = thetas.index(narma_res['theta'])
+    j = betas.index(narma_res['beta'])
+
+    Z[i, j] = mean
+
+im = plt.imshow(
+    Z,
+    origin='lower',
+    aspect='auto',
+    extent=extent,
+    vmin=0
+)
+
+plt.title('NARMA NRMSE')
+plt.xlabel(r'$\beta$')
+plt.ylabel(r'$\theta$')
+plt.xticks(ticks=x_centers, labels=[f'{b:.2f}' for b in betas])
+
+plt.yticks(ticks=y_centers, labels=[f'{t:.3f}' for t in thetas])
+plt.colorbar()
+
+
+# SF
+
+fig = plt.figure()
+
+santa_results = [
+    res for res in results
+    if res['task'] == 'santa' and
+    res['case'] == 'SF-2D' and
+    res['constant_theta'] == False and
+    res['every_second'] == False
+]
+
+betas = sorted({p['beta'] for p in santa_results})
+thetas = sorted({p['theta'] for p in santa_results})
+
+n_beta = len(betas)
+n_theta = len(thetas)
+
+santa_results = sorted(santa_results, key=lambda x: (x['theta'], x['beta']))
+
+Z = np.zeros((n_theta, n_beta))
+
+for santa_res in santa_results:
+    file = build_filename(santa_res)
+
+    mean = np.loadtxt(os.path.join(os.getcwd(), 'results', file), delimiter=',').mean()
+
+    i = thetas.index(santa_res['theta'])
+    j = betas.index(santa_res['beta'])
+
+    Z[i, j] = mean
+
+im = plt.imshow(
+    Z,
+    origin='lower',
+    aspect='auto',
+    extent=extent,
+    vmin=0,
+    vmax=(Z.mean() / 4)
+)
+
+print(f'SF Z mean: {Z.mean()}')
+
+plt.title('Santa Fe NMSE')
+plt.xlabel(r'$\beta$')
+plt.ylabel(r'$\theta$')
+plt.xticks(ticks=x_centers, labels=[f'{b:.2f}' for b in betas])
+
+plt.yticks(ticks=y_centers, labels=[f'{t:.3f}' for t in thetas])
+plt.colorbar()
+
+
 plt.show()
